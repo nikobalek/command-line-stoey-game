79289b2cc23672899427b8ae3cb3cb3c383a185c
nikobalek <nikobalek1381@gmail.com>
2024-05-04 21:04:34 +0330

    project Created!

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+Console.Title = "First Game";
+Console.WriteLine("Hello, World!");
+Console.ForegroundColor = ConsoleColor.Red;
+var name = Console.ReadLine();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("hello " + name + "!");
+Console.ReadKey();
