9659c9503114c1502622bb858e4295740feea0ad
nikobalek <nikobalek1381@gmail.com>
2024-05-08 03:14:43 +0330

    improved story

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
 ﻿Console.Title = "First Game";
 string name;
 
-aiSay("Hello, World!");
+aiSay("Hello, Player! Welcome to this mini game!");
 name = aiAsk("Whats your name?");
-aiSay("hello " + name + "!");
+
+if (name == "Arian")
+    aiSay("Thats a great name you have wise parents player!");
+else
+    aiSay(name + "?" + " who in the right mind would name their child that?!");
+
+aiSay("Anyways " + name+"\nlet's get to the point! you came here for a game and im gonna give it to you!");
+aiAsk("Are you ready?");
+aiSay("Actully I dont give a fuck about what you said lets get to the point!");
+
+
 Console.ReadKey();
 
 void aiSay(string text, ConsoleColor color = ConsoleColor.Green)
--- obj/Debug/net7.0/Learning C#.AssemblyInfo.cs
+++ obj/Debug/net7.0/Learning C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dab0332dd792dcfe7adcdb8105283b18afdf35b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9d1dbf3ac31710b4326ab42fd16f08c72b42fab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
