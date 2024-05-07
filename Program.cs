751ffcafb592b73bc09d6426a723055bfb281ee6
nikobalek <nikobalek1381@gmail.com>
2024-05-07 23:28:27 +0330

    Made function for easier output and input

--- Program.cs
+++ Program.cs
@@ -1,8 +1,23 @@
-﻿Console.ForegroundColor = ConsoleColor.Green;
-Console.Title = "First Game";
-Console.WriteLine("Hello, World!");
-Console.ForegroundColor = ConsoleColor.Red;
-var name = Console.ReadLine();
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("hello " + name + "!");
+﻿Console.Title = "First Game";
+
+aiSay("Hello, World!");
+var name = aiAsk("Whats your name?");
+aiSay("hello " + name + "!");
 Console.ReadKey();
+
+void aiSay(string text)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(text);
+    Console.ForegroundColor = ConsoleColor.White;
+}
+
+string aiAsk(string text)
+{
+    string answear;
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(text);
+    Console.ForegroundColor = ConsoleColor.White;
+    answear = Console.ReadLine();
+    return answear;   
+}
