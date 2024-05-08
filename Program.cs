f66b215e0ec15051d2a0697d93c3299916d0a5e8
nikobalek <nikobalek1381@gmail.com>
2024-05-08 03:36:22 +0330

    added typing animation

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
-﻿Console.Title = "First Game";
+﻿using System.Net.Http.Headers;
+
+Console.Title = "First Game";
 string name;
 
 aiSay("Hello, Player! Welcome to this mini game!");
 name = aiAsk("Whats your name?");
 
 if (name == "Arian")
-    aiSay("Thats a great name you have wise parents player!");
+    aiSay("Thats a great name, you have wise parents player!");
 else
     aiSay(name + "?" + " who in the right mind would name their child that?!");
 
@@ -16,10 +18,10 @@ aiSay("Actully I dont give a fuck about what you said lets get to the point!");
 
 Console.ReadKey();
 
-void aiSay(string text, ConsoleColor color = ConsoleColor.Green)
+async void aiSay(string text, ConsoleColor color = ConsoleColor.Green)
 {
     Console.ForegroundColor = color;
-    Console.WriteLine(text);
+    type(text);
     Console.ResetColor();
 }
 
@@ -27,8 +29,18 @@ string aiAsk(string text, ConsoleColor color = ConsoleColor.Green)
 {
     string answear;
     Console.ForegroundColor = color;
-    Console.WriteLine(text);
+    type(text);
     Console.ResetColor();
     answear = Console.ReadLine();
     return answear;
+}
+
+void type(string text)
+{
+    for (int i = 0; i < text.Length; i++) 
+    {   
+        Console.Write(text[i]);
+        Thread.Sleep(100);
+    }
+    Console.WriteLine();
 }
